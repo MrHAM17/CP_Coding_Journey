3ca8d74f73bc0b64a3eba7f8b5dd2afcb7aa1cff
Harsh Minde <120514233+MrHAM17@users.noreply.github.com>
2023-09-03 22:39:24 +0530

    Create solution_using_set.cpp

---
+++ Love_Babbar_DSA_Sheet/6.      Find Union & Intersection of two sorted arrays/solution_using_set.cpp
@@ -0,0 +1,24 @@
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+       //code here  
+       unordered_set<long long > s;
+       
+       for(int i=0;i<n;i++)
+       {    s.insert(a[i]);     }
+       
+       for(int i=0;i<m;i++)
+       {    s.insert(b[i]);     }
+       
+       return s.size();
+    }
+};
+
+
+// *** L.B. DSA Sheet ***  Q.6/450
+
+// TC = O(n + m),   SC = O(n + m)     
+
+
+
