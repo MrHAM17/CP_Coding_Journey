f10f0c9ea76d17c440c68bb0ad5c9a72c0086afa
Harsh Minde <120514233+MrHAM17@users.noreply.github.com>
2023-09-03 12:10:31 +0530

    Create Case I --> Way 2.cpp

---
+++ Love_Babbar_DSA_Sheet/1.    Reverse the array/Case I --> Way 2.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        int start = 0 ;
+        int end = str.length() -1 ;
+        
+        while( start < end)
+        {
+            char temp = str[end] ;
+            
+            str[end]  = str[start] ;
+            str[start]= temp ;
+            
+            start++ ;
+            end-- ;
+        }
+        
+        return str ;
+    }
+};
+
+
+// *** L.B. DSA Sheet ***  Q.1/450
+
+// TC = O(N),   SC = O(1)                  --> 1st Optimized Approach
+
